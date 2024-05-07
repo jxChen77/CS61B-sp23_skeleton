37cedda85a660520482a4cdbb9a3e79662884675
Chris <jxchen0211@gmail.com>
2024-05-07 10:58:19 -0700

    3/4 of the ListExercises homework.

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,20 +1,50 @@
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        return 0;
+        int sum=0;
+        for (Integer integer : L) {
+            sum += integer;
+        }
+        return sum;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        return null;
+        List<Integer> NewOne = new ArrayList<>();
+        for (int ele : L) {
+            if (ele % 2 == 0) {
+                NewOne.add(ele);
+            }
+        }
+        return NewOne;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        int lengthL1 = L1.size(), lengthL2 = L2.size();
+
+        if (lengthL1 <= lengthL2) {
+            for (int ele : L1) {
+                if (L2.contains(ele)) {
+                    result.add(ele);
+                }
+            }
+        }else{
+            for (int ele : L2) {
+                if (L1.contains(ele)) {
+                    result.add(ele);
+                }
+            }
+
+        }
+        return result;
     }
 
 
